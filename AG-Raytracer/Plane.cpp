a8bed00217a2012bd92a063da12e8f80a03659ac
Luuk van de Wiel <luukvandewiel@hotmail.com>
2016-11-29 15:36:02 +0100

    Added plane. Not sure if it's broken, or if weird visuals are caused because we don't use shadow rays yet (and a plane is infinite).

--- AG-Raytracer/Plane.cpp
+++ AG-Raytracer/Plane.cpp
@@ -1,2 +1,13 @@
 ﻿#include "template.h"
 #include "Plane.h"
+
+void Plane::CheckIntersection(Ray* ray)
+{
+	float denominator = dot(normal, ray->direction);
+	if (denominator != 0)
+	{
+		vec3 displacement = position - ray->origin;
+		ray->t = dot(displacement, normal) / denominator;
+	}
+	return;
+}
--- AG-Raytracer/Scene.cpp
+++ AG-Raytracer/Scene.cpp
@@ -1,11 +1,15 @@
 ﻿#include "template.h"
 #include "Scene.h"
 #include "Sphere.h"
+#include "Triangle.h"
+#include "Plane.h"
 
 Scene::Scene()
 {
 	printf("hallo");
 	camera = new Camera();
 	lights[0] = new Light(vec3(2, 5, 2), 10.0f);
-	primitives[0] = new Sphere(vec3(0, 0, 2), 1.0f);
+	//primitives[0] = new Sphere(vec3(0, 0, 2), 1.0f);
+	//primitives[0] = new Triangle(vec3(0, 0, 2), vec3(2, 0, 2), vec3(0, 2, 2));
+	primitives[0] = new Plane(vec3(2,2, 2), vec3(0, 0, 1));
 }
