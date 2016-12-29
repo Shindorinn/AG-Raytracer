029bae0fe970d7a3b562fef612bf19cfc1080414
Luuk van de Wiel <luukvandewiel@hotmail.com>
2016-12-29 17:29:44 +0100

    Faster AABB-Ray intersection :)

--- AG-Raytracer/Ray.cpp
+++ AG-Raytracer/Ray.cpp
@@ -12,112 +12,25 @@ Ray::Ray(vec3 origin, vec3 direction)
 	sign[2] = (inv_direction.z < 0);
 }
 
-// code from: https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-box-intersection
-//
-////SLOW Intersect
-//bool Ray::Intersects(AABB bounds)
-//{
-//	float tmin, tmax, tymin, tymax, tzmin, tzmax;
-//
-//	if (this->direction.x >= 0) {
-//		tmin = (bounds.min.x - this->origin.x) / this->direction.x;
-//		tmax = (bounds.max.x - this->origin.x) / this->direction.x;
-//	}
-//	else {
-//		tmin = (bounds.max.x - this->origin.x) / this->direction.x;
-//		tmax = (bounds.min.x - this->origin.x) / this->direction.x;
-//	}
-//	if (this->direction.y >= 0) {
-//		tymin = (bounds.min.y - this->origin.y) / this->direction.y;
-//		tymax = (bounds.max.y - this->origin.y) / this->direction.y;
-//	}
-//	else {
-//		tymin = (bounds.max.y - this->origin.y) / this->direction.y;
-//		tymax = (bounds.min.y - this->origin.y) / this->direction.y;
-//	}
-//
-//	if ((tmin > tymax) || (tymin > tmax))
-//		return false;
-//
-//	if (tymin > tmin)
-//		tmin = tymin;
-//	if (tymax < tmax)
-//		tmax = tymax;
-//
-//	if (this->direction.z >= 0) {
-//		tzmin = (bounds.min.z - this->origin.z) / this->direction.z;
-//		tzmax = (bounds.max.z - this->origin.z) / this->direction.z;
-//	}
-//	else {
-//		tzmin = (bounds.max.z - this->origin.z) / this->direction.z;
-//		tzmax = (bounds.min.z - this->origin.z) / this->direction.z;
-//	}
-//
-//	if ((tmin > tzmax) || (tzmin > tmax))
-//		return false;
-//	if (tzmin > tmin)
-//		tmin = tzmin;
-//	if (tzmax < tmax)
-//		tmax = tzmax;
-//
-//	return true;
-//}
-
-// Optimized method
 bool Ray::Intersects(AABB bounds)
 {
-	float tmin, tmax, tymin, tymax, tzmin, tzmax;
+	float tx1 = (bounds.min.x - this->origin.x) * this->inv_direction.x;
+	float tx2 = (bounds.max.x - this->origin.x) * this->inv_direction.x;
 
-	int xindex, yindex, zindex;
+	float tmin = min(tx1, tx2);
+	float tmax = max(tx1, tx2);
 
-	xindex = this->sign[0]; yindex = this->sign[1]; zindex = this->sign[2];
+	float ty1 = (bounds.min.y - this->origin.y) * this->inv_direction.y;
+	float ty2 = (bounds.max.y - this->origin.y) * this->inv_direction.y;
 
-	if (xindex == 0)
-	{
-		tmin = (bounds.min.x - this->origin.x) * this->inv_direction.x;
-		tmax = (bounds.max.x - this->origin.x) * this->inv_direction.x;
-	}
-	else
-	{
-		tmin = (bounds.max.x - this->origin.x) * this->inv_direction.x;
-		tmax = (bounds.min.x - this->origin.x) * this->inv_direction.x;
-	}
+	tmin = max(tmin, min(ty1, ty2));
+	tmax = min(tmax, max(ty1, ty2));
 
-	if (yindex == 0)
-	{
-		tymin = (bounds.min.y - this->origin.y) * this->inv_direction.y;
-		tymax = (bounds.max.y - this->origin.y) * this->inv_direction.y;
-	}
-	else
-	{
-		tymin = (bounds.max.y - this->origin.y) * this->inv_direction.y;
-		tymax = (bounds.min.y - this->origin.y) * this->inv_direction.y;
-	}
+	float tz1 = (bounds.min.z - this->origin.z) * this->inv_direction.z;
+	float tz2 = (bounds.max.z - this->origin.z) * this->inv_direction.z;
 
-	if ((tmin > tymax) || (tymin > tmax))
-		return false;
-	if (tymin > tmin)
-		tmin = tymin;
-	if (tymax < tmax)
-		tmax = tymax;
+	tmin = max(tmin, min(tz1, tz2));
+	tmax = min(tmax, max(tz1, tz2));
 
-	if (zindex == 0)
-	{
-		tzmin = (bounds.min.z - this->origin.z) * this->inv_direction.z;
-		tzmax = (bounds.max.z - this->origin.z) * this->inv_direction.z;
-	}
-	else
-	{
-		tzmin = (bounds.max.z - this->origin.z) * this->inv_direction.z;
-		tzmax = (bounds.min.z - this->origin.z) * this->inv_direction.z;
-	}
-
-	if ((tmin > tzmax) || (tzmin > tmax))
-		return false;
-	if (tzmin > tmin)
-		tmin = tzmin;
-	if (tzmax < tmax)
-		tmax = tzmax;
-
-	return true;
+	return tmax >= tmin && tmax >= 0;
 }
