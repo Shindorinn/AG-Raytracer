07bfcb424884baefdbfbbfebc2f244df3c3b7557
aelex13 <du.a.meijer@googlemail.com>
2017-01-24 09:59:14 +0100

    Deleted old code, can be found in the previous commit.

--- __OLD__AG-Raytracer/Light.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "template.h"
-
-Light::Light(vec3 position, vec3 color)
-{
-	this->position = position;
-	this->color = color;
-}
--- __OLD__AG-Raytracer/Light.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-class Light
-{
-public:
-	vec3 position;
-	vec3 color;
-	Light(vec3 position, vec3 color);
-};
